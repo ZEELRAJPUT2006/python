d2d5b87d70190abc6c1a92b588d58b0426b32825
ZEELRAJPUT202006 <Zeelrajput2006@gmail.com>
2025-08-12 12:22:31 +0530

    datatype

---
+++ class/datatype.py
@@ -0,0 +1,31 @@
+# =====numeric datatype=======
+a = 10
+b = 10.25
+c = 1j+5
+print(type(a))
+print(type(b))
+print(type(c))
+
+# ========= sequence datatype ========================
+name = "hello"
+print(type(name))
+l = ["python","java","php"]
+print(type(l))
+t = ("python","java","hello","world","1","2")
+print(type(t))
+
+# ============= map =======================
+s = {"python","a","b","c","a"}
+print(s)
+print(type(s))
+
+d = {"python":"1","java": "2"}
+print(type(d))
+
+e = True
+print(type(e))
+
+# ============== casting ==================
+a = 10
+b = float(a)
+print(type(b))
--- class/demo.py
+++ class/demo.py
@@ -1,1 +1,14 @@
 print("hello world")
+print('hello " python')
+print("hello ' python")
+print("""
+      hello 
+      python
+      hello
+      tops""")
+print("python","java","hello","database", sep=" ==> ")
+
+print("hello \\\' python")
+
+a = input("enter the number")
+print(a)

fc297c65ec45a21c1589a18f170b2c4c65d6b695
ZEELRAJPUT202006 <Zeelrajput2006@gmail.com>
2025-08-12 12:43:51 +0530

    datatype

---
+++ practice/12-datatype.py
@@ -0,0 +1,38 @@
+# # ====== numeric datatype =======
+# a = 10
+# print(type(a))
+
+# b = 10.10
+# print(type(b))
+
+# c = 5j+4
+# print(type(c))
+
+# # ============== squence datatype =================
+# l = ["python","java","php"]
+# print(type(l))
+
+# t = ("python","java","php","sql","1","2")
+# print(type(t))
+
+# # ================ map ======================
+# s = {"hello","world"}
+# print(type(s))
+
+# d = {"name":"zeel","age":"19"}
+# print(type(d))
+
+# e = True
+# print(type(e))
+
+# # ================ casting ====================
+# z = 10
+# b = str(z)
+# print(type(z))
+
+# print("hello \\' world")
+
+# print("hello \\\\' world")
+
+name = input("enter your good name:")
+print(name)

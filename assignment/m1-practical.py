bb49ea447745b297956806ae96219f7555de2eb2
ZEELRAJPUT202006 <Zeelrajput2006@gmail.com>
2025-09-03 11:45:47 +0530

    3-9

---
+++ assignment/m1-practical.py
@@ -0,0 +1,56 @@
+# print("hello world")
+
+# print("my name is zeel")
+
+# Write a Python program that demonstrates the correct use of indentation, comments, and
+# variables following PEP 8 guidelines.
+# ============================================================================================
+# for i in range(1,5):  indentation
+#     print(i)
+
+# a = 10
+# print(a)  comment single line
+
+# a = 10
+# A_10 = "Hello"  variable
+
+# Practical Example 1: How does the Python code structure work?
+
+# a = 10
+
+#  Practical Example 2: How to create variables in Python?
+
+# id = 101
+# name = "jhon"
+# age = 25
+
+# print(id,name,age)
+
+# Practical Example 3: How to take user input using the input() function
+
+# a = input("enter the value of a")
+# print(a)
+
+# Practical Example 4: How to check the type of a variable dynamically using type()
+
+# a = 10
+# name = "jhon"
+# b = 10.10
+
+# print(type(a))
+# print(type(name))
+# print(type(b))
+
+# Practical Example 5: Write a Python program to find greater and less than a number using
+# if_else.
+
+# num1 = int(input("enter the number1"))
+# num2 = int(input("enter the number1"))
+
+# if num1 > num2:
+#     print(f"{num1} number1 is greater {num2}")
+# else:
+#     print(f"{num1}number is less than{num2}")
+
+# Practical Example 6: Write a Python program to check if a number is prime using if_else.
+

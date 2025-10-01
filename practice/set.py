b1c4349a2e651e86c49a2768c5e706b75eefecff
ZEELRAJPUT202006 <Zeelrajput2006@gmail.com>
2025-10-01 19:11:01 +0530

    1-10

---
+++ practice/set.py
@@ -0,0 +1,32 @@
+s = {10,20,30,40,10}
+# s.clear()
+# s.remove(10)
+# s.add(50)
+# s.pop()
+# s.discard(10)
+print(s)
+# print(type(s))
+
+# print(s.add("hello"))
+# s1 = s.append("hello")
+# print(s1)
+
+s1 = {40,50,60,7}
+# print(s | s1)
+# print(s.union(s1))
+
+# print(s.intersection(s1))
+# print(s & s1)
+# print(s.intersection_update(s1))
+# s2 = s.intersection_update(s1)
+# print(s2)
+
+# print(s.difference(s1))
+# print(s1.difference(s))
+# print(s1.difference_update(s))
+
+# s.update({3})
+# s.add({'h'})
+# print(s)
+
+
